17dbf4f465f185240e664b728ff60a68e7eb4bf6
Vitalii <s31719@pjwstk.edu.pl>
2025-11-25 19:38:22 +0100

    added V1, V2 migrations

---
+++ reserved_products_warehouse/src/main/resources/db/migration/V1__create_database_schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE inventory_item (
+    sku VARCHAR(255) PRIMARY KEY,
+    available INT NOT NULL,
+    reserved INT NOT NULL,
+    version BIGINT NOT NULL
+);
+
+CREATE TABLE domain_event (
+    id BIGSERIAL PRIMARY KEY,
+    sku VARCHAR(255) NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    payload JSONB NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT fk_inventory_item
+        FOREIGN KEY(sku) REFERENCES inventory_item(sku)
+);
---
+++ reserved_products_warehouse/src/main/resources/db/migration/V2__seed_data_in_inventory_item.sql
@@ -0,0 +1,6 @@
+INSERT INTO inventory_item (sku, available, reserved, version) VALUES
+('Iphone-17-pro-max', 50, 10, 1),
+('Logitech-G-Pro-X', 120, 0, 1),
+('NVIDIA-RTX-4070', 30, 2, 1),
+('AMD-Ryzen-7-5800x', 5, 0, 1),
+('Playstation-5', 250, 10, 1);
